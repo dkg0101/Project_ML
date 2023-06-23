71522d43bb07162ebe71094220bbd389ba31cf45
Dhananjay <dkgurav0101@gmail.com>
2023-06-23 08:05:14 +0530

    setup file added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+
+from flask import Flask, render_template, request, jsonify
+import pickle
 
 app = Flask(__name__)
+
 
 @app.route("/" , methods=['GET','POST'])
 def index():
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from typing import List 
+
+PROJECT_NAME='housing-predictor'
+VERSION ='0.0.1'
+AUTHOR = 'Dhananjay Gurav'
+DESCRIPTION = 'First end to end ml project with deployments'
+PACKAGES = ["housing"]
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file 
+    return this function is going to return a list which  contain name 
+    of libraries mentioned in requirements.txt file
+    """
+    with open (REQUIREMENT_FILE_NAME,"r") as requirement_file:
+        return list(requirement_file.readlines())
+
+    
+
+
+
+# def find_packages()
+
+setup(
+    name=PROJECT_NAME,
+    version= VERSION,
+    author = AUTHOR,
+    description=DESCRIPTION,
+    packages=  PACKAGES ,
+    install_requires= get_requirements_list()
+)
+
+
+
+if __name__ == "__main__":
+    requirements_list = get_requirements_list()
+
+    for library in requirements_list:
+        print(library)
